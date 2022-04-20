c062c1f96e218a00576363e5280d3dd64b0928e4
Mareike <mareike.brandt@hotmail.de>
2022-04-20 22:46:44 +0900

    feat: added folder icon to layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,26 @@
 import React from "react";
 import "./main.css";
-import { navbar } from "../components/navbar";
+import Folder from "../assets/folder.png";
 
 export const Main = () => {
   return (
     <div className="main">
       <div className="column1">
+        <img src={Folder} alt="folder" className="folder" />
         <p>trash</p>
       </div>
       <div className="column2">
+        <img src={Folder} alt="folder" className="folder" />
         <p>projects</p>
+        <img src={Folder} alt="folder" className="folder" />
         <p>aboutMe</p>
       </div>
       <div className="column3">
+        <img src={Folder} alt="folder" className="folder" />
         <p>Programme</p>
+        <img src={Folder} alt="folder" className="folder" />
         <p>Jakobs10UsabilityHeuristi...Posters4</p>
+        <img src={Folder} alt="folder" className="folder" />
         <p>Bildschirmfotos</p>
       </div>
     </div>
